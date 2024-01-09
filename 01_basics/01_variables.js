5d1191cc1eba2976ea43fccac8926e2bbebefd21
Amit Upadhyay <amitu@cdac.in>
2024-01-09 16:45:31 +0530

    lesson 4

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123444
+let accountEmail = "amit@abc.com"
+var accountPwd =  "2344489"
+accountCity = "Mumbai"
+let accountState;
+
+accountEmail = "werhhh@cffd"
+accountPwd = "346rt65"
+accountCity = "Bengaluru"
+
+/*
+Please do not use var
+because of issue of block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState]);
