31d5dd150e805e8c16abfd80b98996c68d021717
Amit Upadhyay <amitu@cdac.in>
2024-01-09 17:51:17 +0530

    lesson 5

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 +3 ) we cant use this in nodejs
+
+let name = "Abc"
+let age = 18
+let isAvailable = false;
+
+//number => 2 ^ 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null)       //object
+console.log(typeof undefined)  //undefined
+
+
+
